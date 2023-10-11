ae345b133847df11d609149d6cd8a5aeddbc5c3d
EnMishpat <137240762+EnMishpat@users.noreply.github.com>
2023-10-10 20:34:43 -0400

    making some head way

--- classProjects/dateCalculator.js
+++ classProjects/dateCalculator.js
@@ -14,5 +14,18 @@ function showDates(){
     let firstDateMil = firstDateValue.getTime();
     let secondDateMil =secondDateValue.getTime();
     let calculatedDif = secondDateMil - firstDateMil;
-    alert(calculatedDif/1000/60/60/24/30);
+   // calculating the difference in years
+    let yearDif = Math.round(calculatedDif/1000/60/60/24/365);
+    // displaying the difference in years inside the paragraph
+    years.innerText = yearDif;
+    // calculating the difference in months
+    let monthDif = Math.round(calculatedDif/1000/60/60/24/30);
+    // displaying the difference in months inside the paragraph
+    months.innerText = monthDif;
+    // calculating the difference in days
+    let daysDif = Math.round(calculatedDif/1000/60/60/24);
+    // displaying the difference in days inside the paragraph
+    days.innerText = daysDif;
+
+   
 };
